c61bc500856ebca8bfb4c5374e920ae8d7dedb25
Sameer Banarjee <sameerbanarjee1146@gmail.com>
2025-04-02 10:25:06 +0000

    comparision of data types

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,25 @@
+// ARITHMETIC OPERATIONS COMPARISION
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+
+// In JavaScript different data types can be compared using in built conversion
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// TRICKY THINGS 
+// avoid comparing two different datatypes in javascript
+// in typescript this feature is already added, there u can't compare two different data types
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
