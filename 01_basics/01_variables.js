fe1f6e5d1e9ca3caf510f20b5a5ff0a0bfd6946b
Sameer Banarjee <sameerbanarjee1146@gmail.com>
2025-04-23 06:01:00 +0000

    variables & scope

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,9 @@ let accountEmail = "sameer145@gmail.com"
 var accountPassword = "1245"
 accountCity = "Bhagalpur"
 
+// VAR declares variables with global scope and allows re-declaration within the same scope
+// LET & CONST declares variables with block scope and cannot be re-declaration within same scope.
+
 let accountState;
 // In JAVASCRIPT semicolon(;) doesn't matter either u use it or not it'll not make any change.
 
@@ -21,3 +24,7 @@ accountCity = "Bangalore"
 
 // to print multiple values at the same time we use tables 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// HOISTING IN JS (VAR)
+// LET & CONST IN TEMPORAL DEADZONE
+// EXECUTION  - TWO PARTS MEMORY & CODE
